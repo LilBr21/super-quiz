fcac091930a00c8fe111f07c6c324841c43ce800
Halina <halina.syposz@gmail.com>
2021-07-09 18:38:02 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         lets go
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
